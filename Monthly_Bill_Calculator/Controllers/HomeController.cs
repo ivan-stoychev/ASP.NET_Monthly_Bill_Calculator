53efce7216fbc69fd8d70e3f5ffeb64b2e7f5228
Ivan Stoychev <ivan_veselinov_stoychev@outlook.com>
2026-02-12 15:44:17 +0200

    Add project files.

---
+++ Monthly_Bill_Calculator/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Monthly_Bill_Calculator.Models;
+
+namespace Monthly_Bill_Calculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Monthly_Bill_Calculator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Monthly_Bill_Calculator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
