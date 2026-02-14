fd33402b5ba966c2daab3d290e216f0452db56b6
Ivan Stoychev <ivan_veselinov_stoychev@outlook.com>
2026-02-14 12:37:27 +0200

    Add empty file MonthController.cs

---
+++ Monthly_Bill_Calculator/Controllers/MonthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Monthly_Bill_Calculator.Controllers
+{
+    public class MonthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.Ok("I am at Month Controller");
+        }
+    }
+}
