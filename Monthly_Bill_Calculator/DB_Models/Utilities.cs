377a1ccc9a88fd99e001175fad21ff2e7eabf88f
Ivan Stoychev <ivan_veselinov_stoychev@outlook.com>
2026-02-16 00:02:19 +0200

    Removed [Required] attribute from Utilities class.

--- Monthly_Bill_Calculator/DB_Models/Utilities.cs
+++ Monthly_Bill_Calculator/DB_Models/Utilities.cs
@@ -6,11 +6,9 @@ namespace Monthly_Bill_Calculator.DB_Models
     using static ModelValidation;
     public abstract class Utility
     {
-        [Required(ErrorMessage = "Entering consumption is required.")]
         [Range(ConsumptionMin, ConsumptionMax, ErrorMessage = "Consumption must be in the range of 0.0001 - 99999999")]
         public double Consumption { get; set; }
 
-        [Required(ErrorMessage = "Entering a price per unit is required.")]
         [Range(typeof(decimal), PriceMin, PriceMax, ErrorMessage = "price per unit must be in the range of 0.0001 - 99999")]
         public decimal Price { get; set; }
 
