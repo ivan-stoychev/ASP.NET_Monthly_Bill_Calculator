9e348280e5532941b0caac329b224c6fc4f312fe
Ivan Stoychev <ivan_veselinov_stoychev@outlook.com>
2026-02-16 21:21:21 +0200

    Added Models/SummartViewModel.cs

---
+++ Monthly_Bill_Calculator/Models/SummaryViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Monthly_Bill_Calculator.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace Monthly_Bill_Calculator.Models
+{
+    using static ModelValidation;
+    public class UtilitySummary
+    {
+        public double? AvgConsumption { get; set; }
+        public decimal AvgPrice { get; set; }
+
+        public double? TotalConsumption { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+
+    public class SummaryViewModel
+    {
+        [Required]
+        [Range(MinYear, MaxYear)]
+        public int StartYear { get; set; }
+
+        [Required]
+        [Range(MinMonth, MaxMonth)]
+        public int StartMonth { get; set; }
+
+        [Required]
+        [Range(MinYear, MaxYear)]
+        public int EndYear { get; set; }
+
+        [Required]
+        [Range(MinMonth, MaxMonth)]
+        public int EndMonth { get; set; }
+
+        // Stores the average consumption and price for each utility type, as well as the total consumption and price for the specified period.
+        public Dictionary<string, UtilitySummary>? Utilities { get; set; }
+    }
+}
