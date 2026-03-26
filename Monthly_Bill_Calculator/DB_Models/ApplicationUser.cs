570940b9b32aff40533c5883cb390ef9badcd095
Ivan Stoychev <ivan_veselinov_stoychev@outlook.com>
2026-03-26 17:22:53 +0200

    Added ApplicationUser DB_Model

---
+++ Monthly_Bill_Calculator/DB_Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Monthly_Bill_Calculator.DB_Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Monthly_Bill_Calculator.DB_MModels
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [MaxLength(100)]
+        public string? FullName { get; set; }
+
+        [MaxLength(200)]
+        public string? Address { get; set; }
+
+        // Optional: link to bills
+        public ICollection<Bill> Bills { get; set; } = new List<Bill>();
+
+        // Optional: link to payments
+        public ICollection<Payment> Payments { get; set; } = new List<Payment>();
+    }
+}
