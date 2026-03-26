5975fcf94ddf5676b2ae4ab4f3a9d9e5f6735d71
Ivan Stoychev <ivan_veselinov_stoychev@outlook.com>
2026-03-26 17:11:46 +0200

    Added Bill and Payment DB_Models

---
+++ Monthly_Bill_Calculator/DB_Models/Bill.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Monthly_Bill_Calculator.DB_Models
+{
+    public class Bill
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        public DateTime DueDate { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Amount { get; set; }
+
+        public bool IsPaid { get; set; } = false;
+
+        [Required]
+        public string UserId { get; set; } = null!;
+        public ApplicationUser User { get; set; } = null!;
+
+        public ICollection<Payment> Payments { get; set; } = new List<Payment>();
+
+        public string? Description { get; set; }
+
+    }
+}
