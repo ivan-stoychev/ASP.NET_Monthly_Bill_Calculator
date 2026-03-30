f9693b2d28f973d1519fa454a9c4789c76676992
Ivan Stoychev <ivan_veselinov_stoychev@outlook.com>
2026-03-30 19:36:04 +0300

    Added seed info for Admin account and simplified CalcAppUser.

--- Monthly_Bill_Calculator/DB_Models/CalcAppUser.cs
+++ Monthly_Bill_Calculator/DB_Models/CalcAppUser.cs
@@ -5,12 +5,6 @@ namespace Monthly_Bill_Calculator.DB_Models
 {
     public class CalcAppUser : IdentityUser
     {
-        [MaxLength(100)]
-        public string? FullName { get; set; }
-
-        [MaxLength(200)]
-        public string? Address { get; set; }
-
         public ICollection<Bill> Bills { get; set; } = new List<Bill>();
 
         public ICollection<Payment> Payments { get; set; } = new List<Payment>();
---
+++ Monthly_Bill_Calculator/Data/SeedData/SeedAdminRole.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Monthly_Bill_Calculator.DB_Models;
+
+namespace Monthly_Bill_Calculator.Data.SeedData
+{
+    public static class SeedAdminRole
+    {
+        public static async Task SeedAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<CalcAppUser>>();
+
+            string[] roles = { "Admin", "User" };
+
+            foreach (string role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                    await roleManager.CreateAsync(new IdentityRole(role));
+            }
+
+            string adminUsername = "SuperAdmin";
+            string adminPassword = "SuperSecret";
+
+            CalcAppUser admin = await userManager.FindByNameAsync(adminUsername);
+
+            if (admin == null)
+            {
+                admin = new CalcAppUser
+                {
+                    UserName = adminUsername,
+                };
+
+                IdentityResult result = await userManager.CreateAsync(admin, adminPassword);
+
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "Admin");
+                }
+            }
+        }
+    }
+}
