2b144e6b97b6c789d285bef74bec68630a89fd08
Ivan Stoychev <ivan_veselinov_stoychev@outlook.com>
2026-04-02 22:42:29 +0300

    Created BillController

---
+++ Monthly_Bill_Calculator/Areas/Admin/Controllers/BillController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Monthly_Bill_Calculator.DB_Models;
+using Monthly_Bill_Calculator.Data;
+
+namespace Monthly_Bill_Calculator.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class BillController : Controller
+    {
+        private readonly CalcAppDbContext _context;
+
+        public BillController(CalcAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var bills = await _context.Bills
+                .Include(b => b.User)
+                .OrderByDescending(b => b.Id)
+                .ToListAsync();
+
+            return View(bills);
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["Users"] = _context.Users.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Bill bill)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewData["Users"] = _context.Users.ToList();
+                return View(bill);
+            }
+
+            _context.Bills.Add(bill);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var bill = await _context.Bills.FindAsync(id);
+            if (bill == null)
+                return NotFound();
+
+            ViewData["Users"] = _context.Users.ToList();
+            return View(bill);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Bill bill)
+        {
+            if (id != bill.Id)
+                return NotFound();
+
+            if (!ModelState.IsValid)
+            {
+                ViewData["Users"] = _context.Users.ToList();
+                return View(bill);
+            }
+
+            _context.Update(bill);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bill = await _context.Bills
+                .Include(b => b.User)
+                .FirstOrDefaultAsync(b => b.Id == id);
+
+            if (bill == null)
+                return NotFound();
+
+            return View(bill);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var bill = await _context.Bills.FindAsync(id);
+            if (bill != null)
+            {
+                _context.Bills.Remove(bill);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
